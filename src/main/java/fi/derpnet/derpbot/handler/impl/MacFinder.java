a3eb2e9b12bf70e5cd8e758a3cb0cee4d9ebb641
Scrin <scrin@localhost>
2016-06-22 21:39:48 +0300

    Improved MAC address search

--- src/main/java/fi/derpnet/derpbot/handler/impl/MacFinder.java
+++ src/main/java/fi/derpnet/derpbot/handler/impl/MacFinder.java
@@ -10,16 +10,16 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MacFinder implements SimpleMessageHandler {
-    
+
     private static Map<String, String> macs;
-    
+
     @Override
     public synchronized void init(MainController controller) {
         if (macs == null) {
             macs = new LinkedHashMap<>();
             InputStream is = this.getClass().getResourceAsStream("/macs.txt");
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            br.lines().forEach(line -> macs.put(line.toLowerCase(), line));
+            br.lines().forEach(line -> macs.put(line.toLowerCase().replace(":", ""), line));
         }
     }
 
@@ -27,18 +27,23 @@ public class MacFinder implements SimpleMessageHandler {
     public String getCommand() {
         return "!mac";
     }
-    
+
     @Override
     public String getHelp() {
         return "Searches for MAC addresses and their manufacturers";
     }
-    
+
     @Override
     public String handle(String sender, String recipient, String message, IrcConnector ircConnector) {
         if (!message.startsWith("!mac ") || message.length() < 8) {
             return null;
         }
-        String search = message.substring(5).toLowerCase();
-        return macs.entrySet().stream().filter(e -> e.getKey().contains(search)).map(e -> e.getValue()).findAny().orElse("Not found");
+        String search = message.substring(5).toLowerCase().replace(":", "").replace(".", "");
+        String ret = macs.entrySet().stream().filter(e -> e.getKey().contains(search)).map(e -> e.getValue()).findAny().orElse(null);
+        if (ret != null) {
+            return ret;
+        }
+        String trimmedSearch = search.substring(0, 6);
+        return macs.entrySet().stream().filter(e -> e.getKey().contains(trimmedSearch)).map(e -> e.getValue()).findAny().orElse("Not found");
     }
 }
