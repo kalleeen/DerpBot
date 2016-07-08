d88ef2cc1ea067dbf8d23e42714f23e8ecc05b42
Thomas Gustafsson <Thomas Gustafsson>
2016-07-08 22:48:02 +0300

    Add reittiopas

---
+++ src/main/java/fi/derpnet/derpbot/handler/impl/SimpleMultiLineMessageHandler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.derpnet.derpbot.handler.impl;
+
+/**
+ *
+ * @author kalle
+ */
+interface SimpleMultiLineMessageHandler {
+    
+}
