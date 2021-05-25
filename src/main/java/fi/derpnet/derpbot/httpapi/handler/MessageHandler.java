91bf6b45822474d00f707a5cc0d037c68bb65412
Thomas Gustafsson <kalleeen@hotmail.com>
2021-05-25 17:56:02 +0300

    Better handling of multiline messages in http-api when using Matrix

--- src/main/java/fi/derpnet/derpbot/httpapi/handler/MessageHandler.java
+++ src/main/java/fi/derpnet/derpbot/httpapi/handler/MessageHandler.java
@@ -4,8 +4,6 @@ import com.google.gson.Gson;
 import fi.derpnet.derpbot.bean.MatrixMessage;
 import fi.derpnet.derpbot.connector.MatrixConnector;
 import fi.derpnet.derpbot.controller.MainController;
-import java.util.stream.Stream;
-import org.apache.commons.lang3.StringUtils;
 
 public class MessageHandler {
 
@@ -19,12 +17,9 @@ public class MessageHandler {
         MessagePayload msg = new Gson().fromJson(str, MessagePayload.class);
         if (msg != null && msg.message != null) {
             controller.getConnectors().stream()
-                    .filter(c -> c instanceof MatrixConnector)
+                    .filter(MatrixConnector.class::isInstance)
                     .findAny()
-                    .ifPresent(connector -> Stream.of(msg.message.split("\n"))
-                    .filter(StringUtils::isNotBlank)
-                    .map(message -> new MatrixMessage(message, msg.recipient))
-                    .forEach(connector::send));
+                    .ifPresent(connector -> connector.send(new MatrixMessage(msg.message, msg.recipient)));
         }
     }
 
